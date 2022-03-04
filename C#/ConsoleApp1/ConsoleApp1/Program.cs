f46890667d999b72eb2ef3d16bef1ed78b2366c1
1MuRaD12 <tu71tfsws@code.edu.az>
2022-03-05 00:33:54 +0300

    C#1

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] arrey = { "aynure", "cavidl", "edlar", "elvin", "eli" };
+
+              for (int i = 0; i < arrey.Length; i++) { 
+            
+
+
+                if (arrey[i].Length > 5)
+
+                {
+                    Console.WriteLine(arrey[i]);
+                }
+
+
+
+            }    
+
+
+            
+
+        }
+    }
+}
---
+++ C#2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int n = 128, i , sum = 0;
+
+
+            while (n > 0)
+            {
+                i = n % 10;
+                sum = sum + i;
+                n = n / 10;
+                {
+                    Console.WriteLine("sum of digits is {0}", sum);
+                }
+            }
+        }
+    }
+}
